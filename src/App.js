7f2108fbc01352094a3417ff8e26806b1bd056b0
globotix <denesh.siva@globotix.sg>
2021-11-25 13:15:18 +0800

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import * as React from "react"
 import './App.css';
-import {Button, ButtonGroup} from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 import axios from "axios"
 
 
-const {useState} = React;
+const { useState } = React;
 
 function App() {
   const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Learn React</h1>
-          <h6>{counter}</h6>
-          <Button colorScheme="green" onClick={() => {
-            setCounter(counter+1)
-          }}>Increment</Button>
+        <h1>Learn React</h1>
+        <h6>{counter}</h6>
+        <Button colorScheme="green" onClick={() => {
+          setCounter(counter + 1)
+        }}>Increment</Button>
+        <Button colorScheme="blue" onClick={() => {
+          fetchRandomData()
+        }}>Fetch Data</Button>
       </header>
     </div>
   );
+}
+
+const fetchRandomData = () => {
+  axios.get('https://randomuser.me/api')
+    .then(res => {
+      console.log(res);
+      return res;
+    })
+    .catch(err => {
+      console.error(err);
+    })
 }
 
 export default App;
